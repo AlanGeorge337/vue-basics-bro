2deb66ca1a94abad4c7bb47b048be4eb00bdf6b4
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2021-01-12 11:14:17 -0500

    7th commit

---
+++ basics/src/components/cheap_shark.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul id="cheap_shark">
+            <li v-for="item in items" :key="item.message">
+            {{ item.message }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cheap-shark",
+        
+    
+       
+       
+       
+       
+       // name: "array_man",
+        // data() {
+        //     return{
+        //         items: [
+        //         { message: 'Apple' },
+        //         { message: 'Banana' },
+        //         { message: 'Strawberry' },
+        //         { message: 'Orange' },
+        //         { message: 'Watermelon' },
+        //         { message: 'Grape' },
+        //     ]
+        //     }
+        // } 
+    };
+</script>
+
+<style></style>
--- basics/src/views/Props.vue
+++ basics/src/views/Props.vue
@@ -2,25 +2,40 @@
     <div>
         <Child :greeting="greeting" :icecream="icecream" />
         <array_man> </array_man>
+
+        <div id="app">{{ info }}</div>
+
     </div>
 
 </template>
 
 <script>
     import Child from "../components/Child";
-    import array_man from "../components/array_man";
+    import cheap_shark from "../components/cheap_shark";
     
     export default {
-        data(){
-            return {
-                greeting: "Welcome to Props",
-                icecream: ["Chocolate", "Vanilla", "Strawberry", "Pistachio"],
-            };
-        },
+        // data(){
+        //     return {
+        //         greeting: "Welcome to Props",
+        //         icecream: ["Chocolate", "Vanilla", "Strawberry", "Pistachio"],
+        //     };
+        // },
         
         components: {
-            Child, array_man,
+            Child, cheap_shark,
         },
+
+        name: "maps",
+        data () {
+            return {
+            info: null
+            }
+        },
+        mounted () {
+            axios
+            .get('https://countriesnow.space/api/v0.1/countries/flag/images')
+            .then(response => (this.info = response))
+  }
     };
 
 </script>
